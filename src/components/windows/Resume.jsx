7fb3bb1d4ac3ce5dab2472bce574220d501cca61
unknown <kunaldhiman27971@gmail.com>
2026-02-16 22:45:27 +0530

    Changed resume

--- src/components/windows/Resume.jsx
+++ src/components/windows/Resume.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './resume.scss';
 import MacWindow from './MacWindow';
 
-const Resume=({windowName,setWindowState}) =>{
+const Resume = ({ windowName, setWindowState }) => {
   return (
-   <MacWindow windowName={windowName} setWindowState={setWindowState}>
-    <div className="resume-window">
-        <embed src="/resume.pdf" frameborder="0"></embed>
-    </div>
-   </MacWindow>
+    <MacWindow windowName={windowName} setWindowState={setWindowState}>
+      <div className="resume-window">
+        <embed src="/CV.pdf" frameborder="0"></embed>
+      </div>
+    </MacWindow>
   )
 }
 
